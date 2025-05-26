f757ed0dd4e189a72cfda2f994d882f272926f9b
David Chen <lucemia@gmail.com>
2025-05-27 00:14:47 +0800

    deploy while release (#3)
    * deploy while release
    
    * fix #2 release
    
    * fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/typed-ffmpeg-playground/',
   plugins: [react()],
   resolve: {
     alias: {
